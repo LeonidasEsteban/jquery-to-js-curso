64044eaf9d688bac894dcbfbba0e609c961310db
Leonidas Esteban <leonidasesteban@gmail.com>
2018-07-03 18:14:28 -0500

    async await

--- src/js/home.js
+++ src/js/home.js
@@ -64,4 +64,22 @@ fetch('https://randomuser.me/api/dsfdsfsd')
   })
   .catch(function() {
     console.log('algo falló')
-  })
+  });
+
+
+(async function load() {
+  // await
+  // action
+  // terror
+  // animation
+  async function getData(url) {
+    const response = await fetch(url);
+    const data = await response.json()
+    return data;
+  }
+
+  const actionList = await getData('https://yts.am/api/v2/list_movies.json?genre=action')
+  const dramaList = await getData('https://yts.am/api/v2/list_movies.json?genre=drama')
+  const animationList = await getData('https://yts.am/api/v2/list_movies.json?genre=animation')
+  console.log(actionList, dramaList, animationList)
+})()
