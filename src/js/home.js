ad30b0de07a5072a23e1f21663e8ac553e1c1de2
Leonidas Esteban <leonidasesteban@gmail.com>
2018-07-03 17:36:50 -0500

    clase 6 - promesas

--- src/js/home.js
+++ src/js/home.js
@@ -6,3 +6,38 @@ let cambia = "@LeonidasEsteban"
 function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
+
+const getUserAll = new Promise(function(todoBien, todoMal) {
+  // llamar a un api
+  setTimeout(function() {
+    // luego de 3 segundos
+    todoBien('se acabó el tiempo');
+  }, 5000)
+})
+
+const getUser = new Promise(function(todoBien, todoMal) {
+  // llamar a un api
+  setTimeout(function() {
+    // luego de 3 segundos
+    todoBien('se acabó el tiempo 3');
+  }, 3000)
+})
+
+// getUser
+//   .then(function() {
+//     console.log('todo está bien en la vida')
+//   })
+//   .catch(function(message) {
+//     console.log(message)
+//   })
+
+Promise.race([
+  getUser,
+  getUserAll,
+])
+.then(function(message) {
+  console.log(message);
+})
+.catch(function(message) {
+  console.log(message)
+})
