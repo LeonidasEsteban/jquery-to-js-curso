33c990f8b67843f56f5c8513f1664b8de2e7bb32
Leonidas Esteban <leonidasesteban@gmail.com>
2018-07-04 16:35:04 -0500

    clase 16 - formularios

--- src/js/home.js
+++ src/js/home.js
@@ -87,8 +87,25 @@ fetch('https://randomuser.me/api/dsfdsfsd')
       $element.setAttribute(attribute, attributes[attribute]);
     }
   }
+  const BASE_API = 'https://yts.am/api/v2/';
 
-  $form.addEventListener('submit', (event) => {
+  function featuringTemplate(peli) {
+    return (
+      `
+      <div class="featuring">
+        <div class="featuring-image">
+          <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
+        </div>
+        <div class="featuring-content">
+          <p class="featuring-title">Pelicula encontrada</p>
+          <p class="featuring-album">${peli.title}</p>
+        </div>
+      </div>
+      `
+    )
+  }
+
+  $form.addEventListener('submit', async (event) => {
     event.preventDefault();
     $home.classList.add('search-active')
     const $loader = document.createElement('img');
@@ -99,12 +116,15 @@ fetch('https://randomuser.me/api/dsfdsfsd')
     })
     $featuringContainer.append($loader);
 
-
+    const data = new FormData($form);
+    const peli = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
+    const HTMLString = featuringTemplate(peli.data.movies[0]);
+    $featuringContainer.innerHTML = HTMLString;
   })
 
-  const actionList = await getData('https://yts.am/api/v2/list_movies.json?genre=action')
-  const dramaList = await getData('https://yts.am/api/v2/list_movies.json?genre=drama')
-  const animationList = await getData('https://yts.am/api/v2/list_movies.json?genre=animation')
+  const actionList = await getData(`${BASE_API}list_movies.json?genre=action`)
+  const dramaList = await getData(`${BASE_API}list_movies.json?genre=drama`)
+  const animationList = await getData(`${BASE_API}list_movies.json?genre=animation`)
   console.log(actionList, dramaList, animationList)
   function videoItemTemplate(movie) {
     return (
