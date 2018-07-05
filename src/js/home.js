f540825b1aaea4377ff784a5911d1e931d7c21ac
Leonidas Esteban <leonidasesteban@gmail.com>
2018-07-05 17:03:47 -0500

    clase 21 - manejo de errores

--- src/js/home.js
+++ src/js/home.js
@@ -74,8 +74,13 @@ fetch('https://randomuser.me/api/dsfdsfsd')
   // animation
   async function getData(url) {
     const response = await fetch(url);
-    const data = await response.json()
-    return data;
+    const data = await response.json();
+    if (data.data.movie_count > 0) {
+      // aquí se acaba
+      return data;
+    }
+    // si no hay pelis aquí continua
+    throw new Error('No se encontró ningun resultado');
   }
   const $form = document.getElementById('form');
   const $home = document.getElementById('home');
@@ -117,14 +122,20 @@ fetch('https://randomuser.me/api/dsfdsfsd')
     $featuringContainer.append($loader);
 
     const data = new FormData($form);
-    const {
-      data: {
-        movies: pelis
-      }
-    } = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
+    try {
+      const {
+        data: {
+          movies: pelis
+        }
+      } = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
 
-    const HTMLString = featuringTemplate(pelis[0]);
-    $featuringContainer.innerHTML = HTMLString;
+      const HTMLString = featuringTemplate(pelis[0]);
+      $featuringContainer.innerHTML = HTMLString;
+    } catch(error) {
+      alert(error.message);
+      $loader.remove();
+      $home.classList.remove('search-active');
+    }
   })
 
   function videoItemTemplate(movie, category) {
